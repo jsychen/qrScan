99835c9593877d88cadf2364acb4fc9220146d56
chenyun <jsychen@qq.com>
2019-09-12 01:09:51 +0800

    0

---
+++ app/js/main.js
@@ -0,0 +1,177 @@
+import QRReader from './vendor/qrscan.js';
+import { snackbar } from './snackbar.js';
+import styles from '../css/styles.css';
+import isURL from 'is-url';
+
+//If service worker is installed, show offline usage notification
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then(reg => {
+        console.log('SW registered: ', reg);
+        if (!localStorage.getItem('offline')) {
+          localStorage.setItem('offline', true);
+          snackbar.show('App is ready for offline usage.', 5000);
+        }
+      })
+      .catch(regError => {
+        console.log('SW registration failed: ', regError);
+      });
+  });
+}
+window.addEventListener('DOMContentLoaded', () => {
+  //To check the device and add iOS support
+  window.iOS = ['iPad', 'iPhone', 'iPod'].indexOf(navigator.platform) >= 0;
+  window.isMediaStreamAPISupported = navigator && navigator.mediaDevices && 'enumerateDevices' in navigator.mediaDevices;
+  window.noCameraPermission = false;
+
+  var copiedText = null;
+  var frame = null;
+  var selectPhotoBtn = document.querySelector('.app__select-photos');
+  var dialogElement = document.querySelector('.app__dialog');
+  var dialogOverlayElement = document.querySelector('.app__dialog-overlay');
+  var dialogCloseBtnElement = document.querySelector('.app__dialog-close');
+  var scanningEle = document.querySelector('.custom-scanner');
+  var textBoxEle = document.querySelector('#result');
+  var helpTextEle = document.querySelector('.app__help-text');
+  var infoSvg = document.querySelector('.app__header-icon svg');
+  var videoElement = document.querySelector('video');
+  window.appOverlay = document.querySelector('.app__overlay');
+
+  //Initializing qr scanner
+  window.addEventListener('load', event => {
+    QRReader.init(); //To initialize QR Scanner
+    // Set camera overlay size
+    setTimeout(() => {
+      setCameraOverlay();
+      if (window.isMediaStreamAPISupported) {
+        scan();
+      }
+    }, 1000);
+
+    // To support other browsers who dont have mediaStreamAPI
+    selectFromPhoto();
+  });
+
+  function setCameraOverlay() {
+    window.appOverlay.style.borderStyle = 'solid';
+  }
+
+  function createFrame() {
+    frame = document.createElement('img');
+    frame.src = '';
+    frame.id = 'frame';
+  }
+
+  //Dialog close btn event
+  dialogCloseBtnElement.addEventListener('click', hideDialog, false);
+  // dialogOpenBtnElement.addEventListener('click', openInBrowser, false);
+
+  //To open result in browser
+  function openInBrowser() {
+    console.log('Result: ', copiedText);
+    window.open(copiedText, '_blank', 'toolbar=0,location=0,menubar=0');
+    copiedText = null;
+    hideDialog();
+  }
+
+  //Scan
+  var infoObj = getInto();
+  const axios = require('axios');
+  function scan(forSelectedPhotos = false) {
+    if (window.isMediaStreamAPISupported && !window.noCameraPermission) {
+      scanningEle.style.display = 'block';
+    }
+
+    if (forSelectedPhotos) {
+      scanningEle.style.display = 'block';
+    }
+
+    const TITLE = localStorage.getItem('name') || '';
+    const TOKEN = localStorage.getItem('token') || '';
+    document.title = `${TITLE}`;
+    QRReader.scan(result => {
+      axios.defaults.headers.common['Authorization'] = TOKEN;
+      axios.post('https://hw.sagacn.com/api/checkin', { customerId: result })
+        .then(function (res) {
+          res = res.data;
+          textBoxEle.value = res.meta.message;
+          scanningEle.style.display = 'none';
+          dialogElement.classList.remove('app__dialog--hide');
+          dialogOverlayElement.classList.remove('app__dialog--hide');
+        })
+        .catch(function (error) {
+          // handle error
+          alert(JSON.stringify(error));
+        });
+    }, forSelectedPhotos);
+  }
+  document.getElementById('handleBack').onclick = function() {
+    window.location.href = 'https://hw.sagacn.com/app/#/list';
+  };
+
+  // 获取token和展台名
+  function getInto() {
+    let obj = {};
+    const href = location.href;
+    const arr = href.split('?');
+    let infoArr = arr[1].split('&');
+    let token = infoArr[0].split('=');
+    let name = infoArr[1].split('=');
+    obj = {
+      'token': token[1],
+      'name': name[1]
+    };
+    return obj;
+  }
+
+
+  //Hide dialog
+  function hideDialog() {
+    copiedText = null;
+    textBoxEle.value = '';
+
+    if (!window.isMediaStreamAPISupported) {
+      frame.src = '';
+      frame.className = '';
+    }
+
+    dialogElement.classList.add('app__dialog--hide');
+    dialogOverlayElement.classList.add('app__dialog--hide');
+    scan();
+  }
+
+  function selectFromPhoto() {
+    //Creating the camera element
+    var camera = document.createElement('input');
+    camera.setAttribute('type', 'file');
+    camera.setAttribute('capture', 'camera');
+    camera.id = 'camera';
+    window.appOverlay.style.borderStyle = '';
+    selectPhotoBtn.style.display = 'block';
+    createFrame();
+
+    //Add the camera and img element to DOM
+    var pageContentElement = document.querySelector('.app__layout-content');
+    pageContentElement.appendChild(camera);
+    pageContentElement.appendChild(frame);
+
+    //Click of camera fab icon
+    selectPhotoBtn.addEventListener('click', () => {
+      scanningEle.style.display = 'none';
+      document.querySelector('#camera').click();
+    });
+
+    //On camera change
+    camera.addEventListener('change', event => {
+      if (event.target && event.target.files.length > 0) {
+        frame.className = 'app__overlay';
+        frame.src = URL.createObjectURL(event.target.files[0]);
+        if (!window.noCameraPermission) scanningEle.style.display = 'block';
+        window.appOverlay.style.borderColor = 'rgb(62, 78, 184)';
+        scan(true);
+      }
+    });
+  }
+});
